87fc6f24b9bede27b1fd5becc7bd4828e0b2ddf7
Bisanz Lab OT2 <65199863+BisanzLab@users.noreply.github.com>
2025-04-23 12:25:35 -0400

    Update BeadCleanup_96well.py

--- OT2_scripts/BeadCleanup/BeadCleanup_96well.py
+++ OT2_scripts/BeadCleanup/BeadCleanup_96well.py
@@ -31,14 +31,21 @@ def add_parameters(parameters):
 	parameters.add_str(
 		variable_name="PlateType",
 		display_name="Plate Type",
-		default="biorad_96_wellplate_200ul_pcr",
+		default="nest_96_wellplate_100ul_pcr_full_skirt",
 		choices=[
-			{"display_name": "Biorad 96 (NOTE NO VWR)", "value": "biorad_96_wellplate_200ul_pcr"},
+			{"display_name": "Biorad 96/VWR 96", "value": "biorad_96_wellplate_200ul_pcr"},
 			{"display_name": "Nest 0.1mL 96 well plate", "value": "nest_96_wellplate_100ul_pcr_full_skirt"}
 		],
 		description=("What type of plate is the DNA in?")
 	)
-
+	parameters.add_int(
+		variable_name="MagOffset",
+		display_name="Magnet Offset",
+		default=-12,
+		minimum=-20,
+		maximum=20,
+		description=("Magnet height offset")
+	)
 	parameters.add_int(
 		variable_name="ReactionVolume",
 		display_name="Reaction Volume",
@@ -82,7 +89,7 @@ def add_parameters(parameters):
 	parameters.add_float(
 		variable_name="CaptureDepth",
 		display_name="Capture Depth",
-		default=1,
+		default=2,
 		minimum=0,
 		maximum=5,
 		description=("How far from bottom of well should the tip be in mm?")
@@ -100,17 +107,19 @@ def run(protocol: protocol_api.ProtocolContext):
 	Waste = protocol.load_labware('nest_1_reservoir_195ml', 3)
 	
 	# set up tips
-	Ntips = ((protocol.params.EndingColumn - protocol.params.StartingColumn) + 1) * 8 * 4 / 96
+	Ntips = ((protocol.params.EndingColumn - protocol.params.StartingColumn) + 1) * 8 * 5 / 96
 	Ntips = int(Ntips) + (1 if Ntips > int(Ntips) else 0) # round up to nearest box
 	
 	protocol.comment("Using " + str(Ntips) + " boxes of tips")
 	Tips = [protocol.load_labware('opentrons_96_filtertiprack_200ul', slot) for slot in range(1+6, Ntips+7)] # start loading at position 7 (6 + 1)
 	mp200 = protocol.load_instrument('p300_multi_gen2', 'right', tip_racks=Tips)
-	
+	Magmodule.disengage()
+
 	
 	# add beads to all 
 	protocol.comment("Adding " + str(protocol.params.BeadRatio * protocol.params.ReactionVolume) + " beads to columns " + str(protocol.params.StartingColumn) + " to " + str(protocol.params.EndingColumn))
-	Magmodule.engage()
+	Magmodule.engage(offset=protocol.params.MagOffset)
+	protocol.delay(minutes=0.1) # wait 1 minute for capture
 	Magmodule.disengage()
 	for col in range(protocol.params.StartingColumn, protocol.params.EndingColumn+1):
 		if col == 1:
@@ -120,21 +129,23 @@ def run(protocol: protocol_api.ProtocolContext):
 			mp200.pick_up_tip(Tips[0]['A'+str(col)])
 		mp200.aspirate(protocol.params.BeadRatio * protocol.params.ReactionVolume, BeadsAndWater['A1'])
 		mp200.dispense(protocol.params.BeadRatio * protocol.params.ReactionVolume, PCRplate['A'+str(col)])
-		mp200.mix(repetitions=10, volume=(protocol.params.BeadRatio * protocol.params.ReactionVolume * 0.5), location=PCRplate['A'+str(col)])
-		mp200.blow_out()
+		mp200.mix(repetitions=10, volume=(protocol.params.BeadRatio * protocol.params.ReactionVolume * 0.9), location=PCRplate['A'+str(col)])
+		#mp200.blow_out()
+		mp200.touch_tip()
 		mp200.return_tip()
 		
-	Magmodule.engage()
-	protocol.delay(minutes=1) # wait 1 minute for capture
+	Magmodule.engage(offset=protocol.params.MagOffset)
+	protocol.delay(minutes=3) # wait 3 minute for capture
 
 	# now loop through column by column processing
 	for col in range(protocol.params.StartingColumn, protocol.params.EndingColumn+1):
 		protocol.comment("Cleaning up " + str(col))
 		mp200.pick_up_tip(Tips[0]['A'+str(col)])
-		mp200.aspirate(protocol.params.BeadRatio * protocol.params.ReactionVolume-2, PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
+		mp200.aspirate((protocol.params.BeadRatio * protocol.params.ReactionVolume) + protocol.params.ReactionVolume , PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
 		mp200.air_gap(volume=10)
-		mp200.dispense(protocol.params.BeadRatio * protocol.params.ReactionVolume+8, Waste['A1'].bottom(20)) # 20 mm off bottom to avoid dirtying tip
-		mp200.drop_tip()
+		mp200.dispense((protocol.params.BeadRatio * protocol.params.ReactionVolume) + protocol.params.ReactionVolume+10, Waste['A1'].bottom(20)) # 20 mm off bottom to avoid dirtying tip
+		mp200.air_gap(volume=10)
+		mp200.return_tip()
 		
 		#do two ethanol washes
 		for i in range(0,2):
@@ -142,15 +153,19 @@ def run(protocol: protocol_api.ProtocolContext):
 			mp200.aspirate(protocol.params.EtOHVolume, EtOH['A1'])
 			mp200.air_gap(volume=10)
 			mp200.dispense(protocol.params.EtOHVolume+10, PCRplate['A'+str(col)])
+			#mp200.blow_out()
+			mp200.touch_tip()
 			protocol.delay(minutes=0.1)
-			mp200.aspirate(protocol.params.EtOHVolume, PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
+			mp200.aspirate(protocol.params.EtOHVolume*1.1, PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
 			mp200.air_gap(volume=10)
-			mp200.dispense(protocol.params.EtOHVolume+10, Waste['A1'].bottom(20)) # 20 mm off bottom to avoid dirtying tip
+			mp200.dispense(protocol.params.EtOHVolume*1.1+10, Waste['A1'].bottom(20)) # 20 mm off bottom to avoid dirtying tip
+			mp200.air_gap(volume=10)
 			# on second wash do extra cleanup
 			if i == 1:
 				mp200.aspirate(protocol.params.EtOHVolume, PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
 				mp200.dispense(protocol.params.EtOHVolume, Waste['A1'].bottom(20)) # 20 mm off bottom to avoid dirtying tip
-			mp200.drop_tip()
+				mp200.air_gap(volume=10)
+			mp200.return_tip()
 		
 		#add water
 		mp200.pick_up_tip()
@@ -158,11 +173,14 @@ def run(protocol: protocol_api.ProtocolContext):
 		Magmodule.disengage()
 		mp200.dispense(protocol.params.ElutionVolume, PCRplate['A'+str(col)])
 		mp200.mix(repetitions=10, volume=(protocol.params.ElutionVolume * 0.5), location=PCRplate['A'+str(col)])
-		mp200.blow_out()
-		Magmodule.engage()
+		#mp200.blow_out()
+		Magmodule.engage(offset=protocol.params.MagOffset)
+		mp200.touch_tip()
+		mp200.return_tip()
 		protocol.delay(minutes=0.5)
+		mp200.pick_up_tip()
 		mp200.aspirate(protocol.params.ElutionVolume-5, PCRplate['A'+str(col)].bottom(protocol.params.CaptureDepth), rate=protocol.params.AspirationSpeed)
 		mp200.dispense(protocol.params.ElutionVolume-5, Elutionplate['A'+str(col)])
-		mp200.drop_tip()
+		mp200.return_tip()
 	
 	
